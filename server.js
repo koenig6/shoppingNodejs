f4ea6f75d128777db25a2b1e13d9dddf85eb5eef
Jody <koenig6@byui.edu>
2019-11-18 22:16:52 -0500

    test

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 const pool = new Pool(
     {
-    connectionString: process.env.DATABASE_URL,
-    ssl:true
+    //connectionString: process.env.DATABASE_URL,
+    //ssl:true
+        const pool = new Pool({connectionString: connectionString});
     });
 
 
@@ -17,7 +18,7 @@ express()
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    .get('/db', async (req, res) =>
+    /*.get('/db', async (req, res) =>
        {
             try{
                 const client = await pool.connect()
@@ -31,6 +32,24 @@ express()
                     console.error(err);
                     res.send("Error " + err);
                 }//end catch
-        })
+        })*/
+
+
+        var sql = "SELECT * FROM store";
+
+        pool.query(sql, function(err, result) {
+            // If an error occurred...
+            if (err) {
+                console.log("Error in query: ")
+                console.log(err);
+            }
+
+            // Log this to the console for debugging purposes.
+            console.log("Back from DB with result:");
+            console.log(result.rows);
+
+
+        });
+
 
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
