752ee0f82b65deb18fd3260d139b8d0c6ed17635
Jody <koenig6@byui.edu>
2019-11-29 16:26:48 -0500

    test

--- models/personModel.js
+++ models/personModel.js
@@ -29,7 +29,7 @@ function insertNewPerson(name, callback)
     console.log('Inside insertNewPerson function in personModel.js');
     //var results = {success:true};
 
-    var sql = 'INSERT INTO person (person_name) VALUES ($1::text)';
+    var sql = 'INSERT INTO person (person_name) VALUES ($1)';
 
     //var params = [name];
     pool.query(sql, function(err, db_insert){
